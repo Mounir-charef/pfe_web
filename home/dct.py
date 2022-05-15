73ea3c791ce74881060246121bf124580d57b3d9
Mounir <polomnr1@gmail.com>
2022-05-15 19:33:03 +0100

    works for now

---
+++ home/dct.py
@@ -0,0 +1,258 @@
+import math
+import numpy as np
+from PIL import Image
+
+
+def dct(block):
+    b = block.copy()
+    for i in range(8):
+        for j in range(8):
+            if i == 0:
+                ci = 1 / math.sqrt(2)
+            else:
+                ci = 1
+            if j == 0:
+                cj = 1 / math.sqrt(2)
+            else:
+                cj = 1
+            s = 0
+            for k in range(8):
+                for l in range(8):
+                    dct1 = b[k, l] * math.cos((2 * k + 1) * i * math.pi / 16) * math.cos((2 * l + 1) * j * math.pi / 16)
+                    s += dct1
+
+            block[i, j] = ci * cj * s / 4
+    return block
+
+
+def zigzag(input):
+    h = 0
+    v = 0
+
+    vmin = 0
+    hmin = 0
+
+    vmax = input.shape[0]
+    hmax = input.shape[1]
+
+    # print(vmax ,hmax )
+
+    i = 0
+
+    output = np.zeros((vmax * hmax), dtype='int16')
+    # ----------------------------------
+
+    while ((v < vmax) and (h < hmax)):
+
+        if ((h + v) % 2) == 0:  # going up
+
+            if (v == vmin):
+                # print(1)
+                output[i] = input[v, h]  # if we got to the first line
+
+                if (h == hmax):
+                    v = v + 1
+                else:
+                    h = h + 1
+
+                i = i + 1
+
+            elif ((h == hmax - 1) and (v < vmax)):  # if we got to the last column
+                # print(2)
+                output[i] = input[v, h]
+                v = v + 1
+                i = i + 1
+
+            elif ((v > vmin) and (h < hmax - 1)):  # all other cases
+                # print(3)
+                output[i] = input[v, h]
+                v = v - 1
+                h = h + 1
+                i = i + 1
+
+
+        else:  # going down
+
+            if ((v == vmax - 1) and (h <= hmax - 1)):  # if we got to the last line
+                # print(4)
+                output[i] = input[v, h]
+                h = h + 1
+                i = i + 1
+
+            elif (h == hmin):  # if we got to the first column
+                # print(5)
+                output[i] = input[v, h]
+
+                if (v == vmax - 1):
+                    h = h + 1
+                else:
+                    v = v + 1
+
+                i = i + 1
+
+            elif ((v < vmax - 1) and (h > hmin)):  # all other cases
+                # print(6)
+                output[i] = input[v, h]
+                v = v + 1
+                h = h - 1
+                i = i + 1
+
+        if ((v == vmax - 1) and (h == hmax - 1)):  # bottom right element
+            # print(7)
+            output[i] = input[v, h]
+            break
+
+    return output
+
+
+def inverse_zigzag(input, vmax, hmax):
+    h = 0
+    v = 0
+
+    vmin = 0
+    hmin = 0
+
+    output = np.zeros((vmax, hmax))
+
+    i = 0
+    # ----------------------------------
+
+    while ((v < vmax) and (h < hmax)):
+        # print ('v:',v,', h:',h,', i:',i)
+        if ((h + v) % 2) == 0:  # going up
+
+            if (v == vmin):
+                # print(1)
+
+                output[v, h] = input[i]  # if we got to the first line
+
+                if (h == hmax):
+                    v = v + 1
+                else:
+                    h = h + 1
+
+                i = i + 1
+
+            elif ((h == hmax - 1) and (v < vmax)):  # if we got to the last column
+                # print(2)
+                output[v, h] = input[i]
+                v = v + 1
+                i = i + 1
+
+            elif ((v > vmin) and (h < hmax - 1)):  # all other cases
+                # print(3)
+                output[v, h] = input[i]
+                v = v - 1
+                h = h + 1
+                i = i + 1
+
+
+        else:  # going down
+
+            if ((v == vmax - 1) and (h <= hmax - 1)):  # if we got to the last line
+                # print(4)
+                output[v, h] = input[i]
+                h = h + 1
+                i = i + 1
+
+            elif (h == hmin):  # if we got to the first column
+                # print(5)
+                output[v, h] = input[i]
+                if (v == vmax - 1):
+                    h = h + 1
+                else:
+                    v = v + 1
+                i = i + 1
+
+            elif ((v < vmax - 1) and (h > hmin)):  # all other cases
+                output[v, h] = input[i]
+                v = v + 1
+                h = h - 1
+                i = i + 1
+
+        if ((v == vmax - 1) and (h == hmax - 1)):  # bottom right element
+            # print(7)
+            output[v, h] = input[i]
+            break
+
+    return output
+
+
+def text(a):
+    ret = ''
+    for b in a:
+        intt = int(b, 2)
+        schr = chr(intt)
+        ret += schr
+    return ret
+
+
+def idct(block):
+    b = block.copy()
+    for i in range(8):
+        for j in range(8):
+            s = 0
+            for u in range(8):
+                for v in range(8):
+                    if (u == 0):
+                        cu = 1 / math.sqrt(2)
+                    else:
+                        cu = 1
+                    if (v == 0):
+                        cv = 1 / math.sqrt(2)
+                    else:
+                        cv = 1
+                    idct = cu * cv * b[u, v] * math.cos((2 * i + 1) * u * math.pi / 16) * math.cos(
+                        (2 * j + 1) * v * math.pi / 16)
+                    s += idct
+            block[i, j] = round(s / 4)
+    return block
+
+
+def rgb2ycbcr(im):
+    xform = np.array([[.299, .587, .114], [-.1687, -.3313, .5], [.5, -.4187, -.0813]])
+    ycbcr = im.dot(xform.T)
+    ycbcr[:, :, [1, 2]] += 128
+    return np.uint8(ycbcr)
+
+
+def ycbcr2rgb(im):
+    xform = np.array([[1, 0, 1.402], [1, -0.34414, -.71414], [1, 1.772, 0]])
+    rgb = im.astype(float)
+    rgb[:, :, [1, 2]] -= 128
+    rgb = rgb.dot(xform.T)
+    np.putmask(rgb, rgb > 255, 255)
+    np.putmask(rgb, rgb < 0, 0)
+    return np.uint8(rgb)
+
+
+def insert(input, msg):
+    num = abs(input[15])
+    binary = bin(num)[2:-1]
+    binary += msg.pop(0)
+    num = int(binary, 2)
+    if (input[15] < 0):
+        num = -num
+    input[15] = num
+    return input
+
+
+def get(input):
+    ret = bin(input[0])[-1]
+    return ret
+
+
+def print_image_from_array(image_array):
+    image_array_copy = image_array.clip(0, 255)
+    image_array_copy = image_array_copy.astype("uint8")
+    img = Image.fromarray(image_array_copy, 'YCbCr')
+    img.show()
+
+
+def PSNR(original, compressed):
+    mse = np.mean((original - compressed) ** 2)
+    if (mse == 0):
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
+    return psnr
---
+++ home/y.py
@@ -0,0 +1,101 @@
+from home.dct import *
+from home.ap import split, toBinary
+
+quantTable = np.array([
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72, 92, 95, 98, 112, 100, 103, 99]
+])
+
+
+def extract(file_name, leng):
+    leng *= 8
+    img = Image.open(file_name)
+    msg = ''
+    width, height = (256,256)
+    width -= width % 8
+    height -= height % 8
+    block_size = 8
+    h = height // block_size
+    w = width // block_size
+    arr = np.array(img)
+    arr = rgb2ycbcr(arr)
+    padded_img = np.zeros((height, width), dtype='int16')
+    padded_img[0:height, 0:width] = arr[0:height, 0:width, 0]
+    for i in range(h):
+        row_ind_1 = i * block_size
+        row_ind_2 = row_ind_1 + block_size
+
+        for j in range(w):
+            if not leng:
+                break
+            leng -= 1
+            col_ind_1 = j * block_size
+            col_ind_2 = col_ind_1 + block_size
+            block = padded_img[row_ind_1: row_ind_2, col_ind_1: col_ind_2].astype('float32')
+            block -= 128
+            block = dct(block)
+            block = np.divide(block, quantTable)
+            block = np.round(block)
+            zig = np.int16(zigzag(block))
+            msg += bin(zig[15])[-1]
+    msg = [msg[i:i + 8] for i in range(0, len(msg), 8)]
+    msg = text(msg)
+    print(msg)
+    return msg
+
+
+def watermarking(file_name, message):
+    img = Image.open(file_name).convert('RGB')
+    msg = message
+    leng = len(msg)
+    # we need to work on the size
+    msg = split(toBinary(msg), 1)
+    width, height = (256,256)
+    width -= width % 8
+    height -= height % 8
+    block_size = 8
+    h = height // block_size
+    w = width // block_size
+    arr = np.array(img)
+    nrr = arr.copy()
+    arr = rgb2ycbcr(arr)
+    padded_img = np.zeros((height, width), dtype='int16')
+    padded_img[0:height, 0:width] = arr[0:height, 0:width, 0]
+    for i in range(h):
+
+        row_ind_1 = i * block_size
+        row_ind_2 = row_ind_1 + block_size
+
+        for j in range(w):
+            col_ind_1 = j * block_size
+            col_ind_2 = col_ind_1 + block_size
+            block = padded_img[row_ind_1: row_ind_2, col_ind_1: col_ind_2].astype('float32')
+            block -= 128
+            block = dct(block)
+            block = np.divide(block, quantTable)
+            block = np.round(block)
+            zig = np.int16(zigzag(block))
+            if msg:
+                zig = insert(zig, msg)
+            block = inverse_zigzag(zig, 8, 8)
+            block = np.multiply(block, quantTable)
+            block = idct(block)
+            block += 128
+            block = block.clip(0, 255)
+            padded_img[row_ind_1: row_ind_2, col_ind_1: col_ind_2] = block
+    for i in range(height):
+        for j in range(width):
+            arr[i, j, 0] = padded_img[i, j]
+    arr = ycbcr2rgb(arr)
+    img = Image.fromarray(arr, 'RGB')
+    return img, PSNR(nrr, arr), leng
+
+
+if __name__ == '__main__':
+    exit('wrong file excuted')
